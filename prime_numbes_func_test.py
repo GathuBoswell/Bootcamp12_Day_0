78d0da6dd1b720cb3aab590e417c606116cec840
GathuBoswell <gathudev@gmail.com>
2016-11-10 15:30:51 +0300

    added a test for when input is a data structure

--- prime_numbes_func_test.py
+++ prime_numbes_func_test.py
@@ -19,7 +19,10 @@ class PrimeNumberstestCase(unittest.TestCase):
     def test_negatve_input(self):
         self.assertEqual(test_func.prime_number_gen(0), 'Only Positve Numbers allowed')
 
-    def test_input_is_not_integer(self):
-        self.assertEqual(test_func.prime_number_gen(''), 'Only Positve Numbers allowed')
+    def test_input_is_not_integer1(self):
+        self.assertEqual(test_func.prime_number_gen(''), 'Only Numbers allowed')
+
+    def test_input_is_not_integer2(self):
+        self.assertEqual(test_func.prime_number_gen([]), 'Only Numbers allowed')
 if __name__ == '__main__':
     unittest.main()
