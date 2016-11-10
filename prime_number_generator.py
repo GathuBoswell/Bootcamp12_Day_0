284e0af4b26acf2d4e2aaabd1185eb1b9b177708
GathuBoswell <gathudev@gmail.com>
2016-11-10 15:50:01 +0300

    added a main to prime_number_generator.py function to for test purposes

--- prime_number_generator.py
+++ prime_number_generator.py
@@ -1,3 +1,15 @@
 
 def prime_number_gen(n):
-    pass
+    primes_list = []
+    for num in range(2, n):
+        if num == 2:
+            primes_list.append(num)
+        else:
+            for i in range(3, num):
+                if num % i == 0:
+                    pass
+            primes_list.append(num)
+    return primes_list
+def main():
+    print(prime_number_gen(10))
+
--- prime_numbes_func_test.py
+++ prime_numbes_func_test.py
@@ -16,13 +16,11 @@ class PrimeNumberstestCase(unittest.TestCase):
     def test_input_0(self):
         self.assertEqual(test_func.prime_number_gen(0), [])
 
-    def test_negatve_input(self):
-        self.assertEqual(test_func.prime_number_gen(0), 'Only Positve Numbers allowed')
-
     def test_input_is_not_integer1(self):
         self.assertEqual(test_func.prime_number_gen(''), 'Only Numbers allowed')
 
     def test_input_is_not_integer2(self):
         self.assertEqual(test_func.prime_number_gen([]), 'Only Numbers allowed')
+
 if __name__ == '__main__':
     unittest.main()
